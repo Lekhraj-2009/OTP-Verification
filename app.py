151f43b32251809b9be5054f62a01e1d7b23b090
Lekhraj-2009 <85119416+Lekhraj-2009@users.noreply.github.com>
2023-12-03 12:18:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def get_otp():
     if verification_check.status == "pending":
         return render_template('otp_error.html')    # Write code here
     else:
-        return redirect("https://project-c272.onrender.com/")
+        return redirect("https://collaborative-document-epwj.onrender.com")
 
 
 if __name__ == "__main__":
